af8dd963fe8e791a7d9cb7630bb5618aa0136a74
Shah <shahdilawar@gmail.com>
2024-06-06 10:20:55 -0700

    added dataframe and timeseries chart

--- 09_Numpy/04_Basic_Panda_MatPlotlib.py
+++ 09_Numpy/04_Basic_Panda_MatPlotlib.py
@@ -37,7 +37,20 @@ def read_specific_columns():
         print(e1)
 
 # Use the Panda dataframe to build a csv report
+def panda_dataframe_demo():
+    panda_df_arr = np.array([[-2.58289208,  0.43014843, -1.24082018, 1.59572603],
+                [ 0.99027828, 1.17150989,  0.94125714, -0.14692469],
+                [ 0.76989341,  0.81299683, -0.95068423, 0.11769564],
+                [ 0.20484034,  0.34784527,  1.96979195, 0.51992837]])
 
+    data_frame = pd.DataFrame(panda_df_arr)
+    separator_block()
+    print(f"Data frame object is : {data_frame}")
+
+    try:
+        data_frame.to_csv('/Users/kader8/learning/python_basics/09_Numpy/pd_generated.csv' , header=True)
+    except Exception as e:
+        print(e)
 
 '''
 Plotting arrays with MatPlotlib
@@ -50,11 +63,26 @@ def plot_1D_array():
     #use show method to display
     plt.show()
 
+#plot timeseries data of two 1D arrays
+def plot_timeseries_data():
+    x = np.linspace(0, 10, 5)
+    y = np.linspace(0, 20, 5)
+
+    #plotting the values
+    #purple line
+    plt.plot(x, y, 'purple')
+    #dots
+    plt.plot(x, y, 'o')
+    plt.show()
+
+
 
 def test_methods():
     read_all_data_from_csv()
     read_specific_columns()
+    panda_dataframe_demo()
     plot_1D_array()
+    plot_timeseries_data()
 
 if __name__ == "__main__":
     test_methods()
