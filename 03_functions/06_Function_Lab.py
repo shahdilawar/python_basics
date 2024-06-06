057b54aa0911b9c99f19f73feb40405202172037
Shah <shahdilawar@gmail.com>
2024-06-06 13:22:09 -0700

    added function annotations examples

--- 03_functions/06_Function_Lab.py
+++ 03_functions/06_Function_Lab.py
@@ -40,3 +40,40 @@ for i in range(0, 10):
     # check value of i is Odd number
     flag = isOdd(i)
     print(f"{i} is Odd : ", flag)
+
+'''
+Function annotations are completely optional metadata information about 
+the types used by user-defined functions.
+    * Annotations are stored in the __annotations__ attribute of the 
+      function as a dictionary and have no effect on any other part of the 
+      function. 
+    * Parameter annotations are defined by a colon after the parameter
+      name, followed by an expression evaluating to the value of the
+      annotation. 
+    * Return annotations are defined by a literal ->, followed by an 
+      expression, between the parameter list and the colon denoting 
+      the end of the def statement. 
+    * The following example has a required argument, an optional 
+      argument, and the return value annotated:
+'''
+
+def meat_and_eggs(meat : str, eggs : str = "organic eggs") -> str:
+    print("-" * 50)
+    #print the associated annotations dictionary.
+    print("function annotations are : \n", meat_and_eggs.__annotations__)
+    print("Argumnets : " , meat, eggs)
+    return meat + " and " + eggs
+#call the function
+print("Function return is : ", meat_and_eggs("mutton"))
+
+#another example with list
+def iterate_over_list(num : list[int]) -> list[int]:
+    target_list = []
+    print("-" * 50)
+    print("function annotations are : \n", iterate_over_list.__annotations__)
+    print("arguments : ", num)
+    for i in num:
+        target_list.append(i)
+    return target_list
+#call the function
+print("Function return is : ", iterate_over_list([1, 2, 3, 5]))
