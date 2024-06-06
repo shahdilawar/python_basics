5a412f99b7784d3736023c93eee9d9fbed11da1c
Shah <shahdilawar@gmail.com>
2024-06-06 09:09:39 -0700

    added methods

--- 09_Numpy/04_Basic_Panda_MatPlotlib.py
+++ 09_Numpy/04_Basic_Panda_MatPlotlib.py
@@ -14,32 +14,47 @@ def separator_block():
 file_path = "/Users/kader8/learning/python_basics/09_Numpy/music.csv"
 
 # read all contents from csv file
-if not os.path.exists(file_path):
-    raise FileNotFoundError(f"The file {file_path} does not exist")
+def read_all_data_from_csv():
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"The file {file_path} does not exist")
 
-#read from the music.csv all the data into ndarray object
-try:
-    x = pd.read_csv(file_path, header = 0).values
-    separator_block()
-    print(f"CSV contents is : {x}")
-    print(f"Type of x is : {type(x)}")
-except Exception as e:
-    print(f"error occured : {e}")
+    #read from the music.csv all the data into ndarray object
+    try:
+        x = pd.read_csv(file_path, header = 0).values
+        separator_block()
+        print(f"CSV contents is : {x}")
+        print(f"Type of x is : {type(x)}")
+    except Exception as e:
+        print(f"error occured : {e}")
 
 # You can also simply select the columns you need:
-separator_block()
-try:
-    y = pd.read_csv(file_path, usecols=['Artist', 'Plays']).values
-    print(f"CSV contents containing Artist and Plays : \n {y}")
-except Exception as e1:
-    print(e1)
+def read_specific_columns():
+    separator_block()
+    try:
+        y = pd.read_csv(file_path, usecols=['Artist', 'Plays']).values
+        print(f"CSV contents containing Artist and Plays : \n {y}")
+    except Exception as e1:
+        print(e1)
+
+# Use the Panda dataframe to build a csv report
+
 
 '''
 Plotting arrays with MatPlotlib
 '''
 #initializing array
-a = np.array([2, 1, 5, 7, 4, 6, 8, 14, 10, 9, 18, 20, 22])
-#plotting the values
-plt.plot(a)
-#use show method to display
-plt.show()
+def plot_1D_array():
+    a = np.array([2, 1, 5, 7, 4, 6, 8, 14, 10, 9, 18, 20, 22])
+    #plotting the values
+    plt.plot(a)
+    #use show method to display
+    plt.show()
+
+
+def test_methods():
+    read_all_data_from_csv()
+    read_specific_columns()
+    plot_1D_array()
+
+if __name__ == "__main__":
+    test_methods()
